42a35ea61b3303d53e6a7ba2c1e4a9f8047612d3
CaroQuiro <132960850+CaroQuiro@users.noreply.github.com>
2025-10-28 00:06:38 -0500

    Delete src directory

--- src/main/java/co/edu/unbosque/ElecSys/LugarTrabajo/ServicioLug/LugarTrabajoRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unbosque.ElecSys.LugarTrabajo.ServicioLug;
-
-import co.edu.unbosque.ElecSys.LugarTrabajo.EntidadLug.LugarTrabajoEntidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LugarTrabajoRepository extends JpaRepository<LugarTrabajoEntidad, Integer> {
-}
